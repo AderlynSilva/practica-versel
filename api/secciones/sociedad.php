7e348344e2444b5cac2b29685a70eead064231f5
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:44:43 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Estados Unidos: ¿Por qué Trump? 10 cifras clave sobre una sociedad rota",
+"autor" => "EL GRAND CONTINENTE",
+"resumen" => "La economía estadounidense está floreciendo. Este año, el Fondo Monetario Internacional espera que el crecimiento estadounidense sea el doble de rápido que el de los demás países del G7.",
 ];
 ?>
