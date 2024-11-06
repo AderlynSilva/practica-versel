ac30fd8574c35fd7b4677e8be35d95da6068f2c5
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:35:13 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Un tsunami político y geopolítico global: busquen la altura",
+"autor" => "DIARIO EL PAIS",
+"resumen" => "La nueva presidencia de Trump amenaza con profundizar la erosión de la democracia, galvanizar a fuerzas ultras y facilitar la victoria de Putin en Ucrania",
 ];
 ?>
