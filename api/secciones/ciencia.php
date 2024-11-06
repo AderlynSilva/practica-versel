15c1561bb9d9db00df08fb78b5fe996fbc1b7977
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:38:41 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "APLLICACIONES EN LA NUBE",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La ciencia confirma un descubrimiento paralizante sobre la humanidad: dos personas se comunicaron en sueños",
+"autor" => "LA NACION",
+"resumen" => "Los expertos en neurotecnología creen que esta capacidad de comunicarse en sueños podría tener importantes aplicaciones en el tratamiento de problemas de salud mental y en el desarrollo de habilidades.",
 ];
 ?>
