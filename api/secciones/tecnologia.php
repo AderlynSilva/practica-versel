0851611afeee561b0c16938aea79a851da36422d
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:36:32 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Minsa desarrolla tecnología para detectar medicamentos falsificados mediante una prueba rápida",
+"autor" => "EL PERUANO",
+"resumen" => "Se trata del kit portátil "Hampi Tariki”, que permitirá combatir este delito en el mismo lugar donde se desarrolla el operativo. Inicialmente se distribuirá a seis regiones y las Diris de Lima Metropolitana.",
 ];
 ?>
