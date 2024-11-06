4c2a6ec71a85d0a9c07dabb3cbe5a1ba2647ffd4
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:43:38 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Aprueban dictámenes a favor del empleo del adulto mayor y en reconocimiento de trabajadores",
+"autor" => "CONGRESO",
+"resumen" => "En la séptima sesión ordinaria de la Comisión de Trabajo y Seguridad Social que preside la congresista Edhit Julón Irigoín (APP), se aprobó dos dictámenes de proyectos de ley, el primero enfocado en promover el empleo y trabajo de la persona adulta mayor.",
 ];
 ?>
