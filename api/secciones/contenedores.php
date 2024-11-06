1106c18de2fd36a8e37f5a547f22cd94cb866219
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:47:07 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Seguimos entregando los contendores de Residuos solidos a dirigentes de Centros Poblados",
+"autor" => "PLATAFORMA DEL ESTADO PERUANO",
+"resumen" => "La actual gestión ha mostrado enorme interés por cuidar el medio ambiente, en simultáneo se viene construyendo un moderno vivero municipal.",
 ];
 ?>
