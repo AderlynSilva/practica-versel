b3883309eb30ef76746e0271f864d50dd3680b8f
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:30:40 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Abinader felicita a Trump y dice continuará fortaleciendo "los cálidos y estratégicos vínculos",
+"autor" => "DIARIO LIBRE",
+"resumen" => "El presidente Abinader publicó un mensaje en la red social X diciendo que "el Gobierno dominicano continuará fortaleciendo los cálidos y estratégicos vínculos entre nuestros países",
 ];
 ?>
