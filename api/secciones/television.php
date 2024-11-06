7f34c2c06cbd44b1190a4b489c0de5bf0a42b735
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:41:47 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "América Televisión anuncia nueva competencia para ‘El Gran Chef: Famosos’ de Latina",
+"autor" => "INFO BAE",
+"resumen" => "El canal de Pachacamac prepara un programa de cocina para el próximo año, buscando arrebatarle el liderazgo al popular show de gastronomía",
 ];
 ?>
