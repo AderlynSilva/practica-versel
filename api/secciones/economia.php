dfc8b1be7bcfad24d72441c240bc709306352d38
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:34:11 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Precios en alza del oro permitirían seguir recuperando la recaudación",
+"autor" => "DIARIO GESTION",
+"resumen" => "Hasta setiembre, los ingresos tributarios del Gobierno central completaron cuatro meses consecutivos al alza, explicado en parte por mayor aporte de la minería.",
 ];
 ?>
