05c9ccabddbc5e350343f1c200564ecbb5d7748f
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:39:22 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Ministerio de Cultura organizará actividades para la población en el marco de APEC Ciudadano",
+"autor" => "GOBIERNO DEL PERU",
+"resumen" => "Se realizará la Ruta Cultural "Huacatón", el Festival "Afuera Fest Intercultural" con artistas como Eva Ayllón y Ruth Karina, entre otros eventos, desde el 14 de noviembre.",
 ];
 ?>
