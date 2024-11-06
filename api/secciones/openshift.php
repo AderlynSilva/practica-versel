64c998b6ad7785e2577d529a2c79533726fa07a3
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:47:47 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Openshift AI y RHEL AI: sinergia entre inteligencia artificial y código abierto",
+"autor" => "RESLLER",
+"resumen" => "En el marco del Red Hat Summit Connect.2024, la firma convocó a socios, aliados y clientes para que conocieran de primera mano las innovaciones del desarrollador.",
 ];
 ?>
