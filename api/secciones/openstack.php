5a858307b43faa7de2e53c042d871654509b4f1c
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:45:36 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenStack: la nube Open Source está madura, pero se enfrenta a desafíos",
+"autor" => "OPEN STACK",
+"resumen" => "Se celebra estos días en Barcelona el OpenStack Summit, una cita obligada para expertos de todo el mundo en torno a esta tecnología Open Source para la creación de infraestructuras de servicios en la nube que prácticamente se ha convertido ya en un estándar en el sector.",
 ];
 ?>
