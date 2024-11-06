d20d5658a365144abfebac34587cda1f015838a4
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:46:17 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Brecha masiva en Git: 15,000 Credenciales Robadas y 10,000 Repositorios Privados en Riesgo",
+"autor" => "GIT",
+"resumen" => "Un grupo de investigadores en ciberseguridad ha descubierto una campaña masiva que se aprovecha de configuraciones inseguras de Git.",
 ];
 ?>
