8b39747326ec1f463be8f2639c06486f25ab3721
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:40:37 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Pio Dávila: "La gente que maneja el club sabe hacer dinero, pero de fútbol saben poco",
+"autor" => "OVIACION.PE",
+"resumen" => "Pio Dávila, expresidente de Alianza Lima, en diálogo con 'Radio Ovación', no se guardó nada y habló de la temporada del cuadro blanquiazul, la que calificó como mala, sin embargo dijo que el fútbol da revanchas, por lo que espera cobrarse la misma el año que viene.",
 ];
 ?>
