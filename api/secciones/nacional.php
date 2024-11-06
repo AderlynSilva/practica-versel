366e69970f16db9425ddb6062cadc8286f920dce
AderlynSilva <silvaaderlyn88@gmail.com>
2024-11-06 11:32:13 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Un muerto y varios heridos a causa de un rayo en un partido de fútbol en Perú",
+"autor" => "diario libre",
+"resumen" => "Un futbolista murió a causa de una descarga eléctrica en medio de un partido regional que se celebraba en Huancayo, Perú",
 ];
 ?>
